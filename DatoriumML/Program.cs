c3beb3e401e59cc9aa485b2bf59af6e2fc9aafd5
rebeka.pilsikova <rebeka.pilscikova@visma.com>
2025-03-04 09:14:33 +0200

    afasf

--- DatoriumML/Program.cs
+++ DatoriumML/Program.cs
@@ -15,16 +15,44 @@ void ClassifySingleImage(MLContext mlContext, ITransformer model)
 {
 }
 
-//ITransformer GenerateModel(MLContext mLContext)
-//{
-//    IEstimator<ITransformer> pipeline = mlContext.Transforms.LoadImages
 
-//}
-
-void DisplayResult(IEnumerable<ImagePrediction> predictions)
+void DisplayResults(IEnumerable<ImagePrediction> predictions)
 {
     foreach (var prediction in predictions)
     {
         Console.WriteLine($"Image: {Path.GetFileName(prediction.ImagePath)} predicted as: {prediction.PredictedLabelValue} with score: {prediction.Score?.Max()} ");
     }
 }
+
+ITransformer GenerateModel(MLContext mLContext)
+{
+    IEstimator<ITransformer> pipeline = mlContext.Transforms.LoadImages(outputColumnName: "input", imageFolder: _imagesFolder, inputColumnName: nameof(ImageData.ImagePath))
+                    .Append(mlContext.Transforms.ResizeImages(outputColumnName: "input", imageWidth: InceptionSettings.ImageWidth, imageHeight: InceptionSettings.ImageHeight, inputColumnName: "input"))
+                    .Append(mlContext.Transforms.ExtractPixels(outputColumnName: "input", interleavePixelColors: InceptionSettings.ChannelsLast, offsetImage: InceptionSettings.Mean))
+                    .Append(mlContext.Model.LoadTensorFlowModel(_inceptionTensorFlowModel).
+                    ScoreTensorFlowModel(outputColumnNames: new[] { "softmax2_pre_activation" }, inputColumnNames: new[] { "input" }, addBatchDimensionInput: true))
+                    .Append(mlContext.Transforms.Conversion.MapValueToKey(outputColumnName: "LabelKey", inputColumnName: "Label"))
+                    .Append(mlContext.MulticlassClassification.Trainers.LbfgsMaximumEntropy(labelColumnName: "LabelKey", featureColumnName: "softmax2_pre_activation"))
+                    .Append(mlContext.Transforms.Conversion.MapKeyToValue("PredictedLabelValue", "PredictedLabel"))
+                    .AppendCacheCheckpoint(mlContext);
+
+    IDataView trainingData = mlContext.Data.LoadFromTextFile<ImageData>(path: _trainTagsTsv, hasHeader: false);
+    ITransformer model = pipeline.Fit(trainingData);
+
+    IDataView testData = mlContext.Data.LoadFromTextFile<ImageData>(path: _testTagsTsv, hasHeader: false);
+    IDataView predictions = model.Transform(testData);
+
+    IEnumerable<ImagePrediction> imagePredictionData = mlContext.Data.CreateEnumerable<ImagePrediction>(predictions, true);
+    DisplayResults(imagePredictionData);
+
+    MulticlassClassificationMetrics metrics =
+    mlContext.MulticlassClassification.Evaluate(predictions,
+        labelColumnName: "LabelKey",
+        predictedLabelColumnName: "PredictedLabel");
+
+    Console.WriteLine($"LogLoss is: {metrics.LogLoss}");
+    Console.WriteLine($"PerClassLogLoss is: {String.Join(" , ", metrics.PerClassLogLoss.Select(c => c.ToString()))}");
+
+    return model;
+}
+
